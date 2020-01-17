d220552f73f7220b0476edb99ee88704a1d85df6
Samantha Ponce <samanthaponce5@gmail.com>
2020-01-17 15:39:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
   new_arr = []
   counter = 0 
   while counter < array.length 
-    array[counter] ** 2
+    new_arr[counter] = array[counter] ** 2
     counter+= 1
   
 end
-square_array(array)
+new_arr
 end
